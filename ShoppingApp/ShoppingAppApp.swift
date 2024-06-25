562eb575aca7fedb8ad1806317ff275094d8193e
Dhruv Lenka <lenkadhruv@gmail.com>
2024-06-25 11:35:30 +0530

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Dhruv Lenka on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
